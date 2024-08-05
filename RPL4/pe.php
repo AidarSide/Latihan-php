8c33041f1e04e1bf3846178a0eb2300aa94d1e6d
AidarSide <141606625+AidarSide@users.noreply.github.com>
2024-08-05 12:01:01 +0700

    Add files via upload

---
+++ RPL4/pe.php
@@ -0,0 +1,53 @@
+<?php 
+    // Variabel
+    echo "Hallo XI RPL 4";
+    echo "<br>";
+    echo "Nama: Rahmad Haidar Hilmy";
+    echo "<br>";
+    echo "Jk: Laki-laki";
+    echo "<br>";
+    echo "Kelas: XI RPL 4";
+    echo "<br>";
+    echo "Alamat: Jl. Segara Anak G7A2 (KOST GRAHA SELARAS)";
+
+
+    echo "<br>";
+    $nama="RAHMAAAAAATTTT ";
+    $uangSaku=1000000;
+
+    echo $nama;
+    echo "<br>";
+    echo "Uang Saku: $uangSaku";
+    echo "<br>";
+
+
+    // Menghitung Luas Segitiga (OPERATOR)
+    $l = 1/2;
+    $a = 5;
+    $t = 7;
+    $hasil= $l * $a * $t;
+
+    echo $hasil;
+    echo "<br>";
+
+    // Kondisi 
+    $harga =8000;
+    if($harga < 5000){
+        echo "Saya akan Beli Kopi";
+    } else if ($harga <= 8000) {
+        echo "Saya akan tawar sampai jadi";
+    } else {
+        echo "Saya tidak punya uang";
+    }
+    echo "<br>";
+
+
+    // FUNCTION
+    function woiii(){
+        echo "BELAJAR MEMBACA";
+    }
+    woiii();
+
+
+
+?>
